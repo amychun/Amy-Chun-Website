1e967931f0b92a1ec24b02758c4dfb878136a372
Amy Chun <amychun@msn.com>
2024-02-12 19:10:06 -0800

    removed server and webpack auto-reloading correctly

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,22 @@
-const path = require("path");
-
 module.exports = {
-  target: "web",
-  entry: ["@babel/polyfill", "./app/main.js"],
   mode: "development",
+  entry: ["./app/main.js"],
   output: {
-    path: __dirname,
-    filename: "./public/bundle.js",
+    path: __dirname + "/public",
+    filename: "bundle.js",
   },
   context: __dirname,
   devtool: "source-map",
+  devServer: {
+    static: {
+      directory: __dirname + "/public",
+    },
+  },
   module: {
     rules: [
       {
-        test: /\.jsx?$/,
-        exclude: /(node_modules|bower_components)/,
+        test: /jsx?$/,
+        exclude: /node_modules/,
         loader: "babel-loader",
         options: {
           presets: ["@babel/preset-env", "@babel/preset-react"],
